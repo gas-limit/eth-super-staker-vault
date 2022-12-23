5556f0a954a97ecb5e355b8d3de4e83d31b0f95b
Josh (0xDream) <100609687+JoshWeb3@users.noreply.github.com>
2022-12-22 19:39:24 -0500

    Update superStaker.sol

--- superStaker.sol
+++ superStaker.sol
@@ -3,7 +3,6 @@ pragma solidity ^0.8.0;
 import "https://github.com/rocket-pool/rocketpool/blob/master/contracts/interface/RocketStorageInterface.sol";
 import "https://github.com/rocket-pool/rocketpool/blob/master/contracts/interface/deposit/RocketDepositPoolInterface.sol";
 import "https://github.com/rocket-pool/rocketpool/blob/master/contracts/interface/token/RocketTokenRETHInterface.sol";
-import "https://github.com/transmissions11/solmate/blob/main/src/tokens/ERC20.sol";
 
 
 
@@ -15,6 +14,10 @@ contract SuperStaker {
     uint totalShares;
     mapping(address => uint) rEthMintedbyContract;
 
+    constructor(address _rocketStorageAddress) {
+        rocketStorage = RocketStorageInterface(_rocketStorageAddress);
+    }
+
     function vault_Deposit() public payable {
         require(msg.value > 1 gwei, "More than 0 pls");
         userShares[msg.sender] += msg.value;
@@ -22,7 +25,7 @@ contract SuperStaker {
     }
 
 
-    function depositRocketPool() external  {
+    function depositRocketPool() internal  {
 
         // Load contracts
         address rocketDepositPoolAddress = rocketStorage.getAddress(keccak256(abi.encodePacked("contract.address", "rocketDepositPool")));
@@ -41,24 +44,121 @@ contract SuperStaker {
         // Update user's balance
         rEthMintedbyContract[msg.sender] += rethMinted;
     }
-//pool contract
-//0x1e19cf2d73a72ef1332c882f20534b6519be0276
+
+    
+
+
+/*    function _convertERC20sToAssets(IERC20[] memory tokens) internal pure returns (IAsset[] memory assets) {
+        // solhint-disable-next-line no-inline-assembly
+        assembly {
+            assets := tokens
+        }
+    }
+*/
+
 
 //balancer vault contract
 //0xBA12222222228d8Ba445958a75a0704d566BF2C8
+
 //pool id
 //0x1e19cf2d73a72ef1332c882f20534b6519be0276000200000000000000000112-0xae78736cd615f374d3085123a210448e74fc6393-0xc02aaa39b223fe8d0a0e5c4f27ead908
 
-    function provideBalancer() external {
+//reth address
+//0x9559aaa82d9649c7a7b220e7c461d2e74c9a3593
+//weth address
+//0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2
+
+
+
+
+    function provideBalancer() internal {
 
     IBalancerVault balancerVault = IBalancerVault(0xBA12222222228d8Ba445958a75a0704d566BF2C8);
 
-    bytes32 rEthPoolID = 0x1e19cf2d73a72ef1332c882f20534b6519be0276000200000000000000000112-0xae78736cd615f374d3085123a210448e74fc6393-0xc02aaa39b223fe8d0a0e5c4f27ead908;
 
-    balancerVault.joinPool();
-    (IERC20[] memory tokens, , ) = IBalancerVault.getPoolTokens(rEthPoolID);
+
+    bytes32 rEthPoolID = balancerVault.getPoolId();
+
+  
+
+    // should calculate amount to avoid slippage using queryJoin https://dev.balancer.fi/resources/query-batchswap-join-exit#queryjoin
+  //  uint256[2] _maxAmountsIn = [uint256.max,uint256.max];
+
+
+
+
+
+    //get assets
+    address wEth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    IERC20 rETH = IERC20(0x9559Aaa82d9649C7A7b220E7c461d2E74c9a3593);
+    uint256 rETHBalance = rETH.balanceOf(address(this));
+
+    IAsset[] memory assets = new IAsset[](2);
+    assets[0] = IAsset(address(rETH));
+    assets[1] = IAsset(wEth);
+
+    //asset amounts
+    uint256 contractEthAmt = address(this).balance;
+    uint[2] memory rEthAndEthAmt = [rETHBalance,contractEthAmt ];
+
+    //joinpool request
+    IBalancerVault.JoinPoolRequest memory joinPoolRequest = IBalancerVault
+    .JoinPoolRequest({
+        assets:assets,
+        maxAmountsIn: new uint256[](2),
+        userData: abi.encode(
+            IBalancerVault.JoinKind.EXACT_TOKENS_IN_FOR_BPT_OUT,
+            rEthAndEthAmt,
+            1
+        ),
+        fromInternalBalance:true
+    });
+    //Joinpool
+    balancerVault.joinPool(
+        rEthPoolID,
+        address(this),
+        payable(address(this)),
+        joinPoolRequest
+    );
 
     }
+
+   function withdrawBalancer() public {
+
+       //withdraw from balancer
+
+      
+       
+   }
+
+
+    //execute strategy
+    function executeStrategy() public {
+
+        depositRocketPool();
+        provideBalancer();
+
+    }
+
+
+
+   // end strategy
+
+   function endStrategy() public {
+
+       //require 1 year has passed
+
+       //withdraw from balancer
+
+       //swap reth for weth
+
+       //burn weth
+
+       //open user withdrawal
+
+   }
+
+
 
 
     
@@ -66,3 +166,39 @@ contract SuperStaker {
 
 
 }
+
+interface IBalancerVault {
+    function joinPool(
+        bytes32 poolId,
+        address sender,
+        address recipient,
+        JoinPoolRequest memory request
+    ) external payable;
+
+    struct JoinPoolRequest {
+        IAsset[] assets;
+        uint256[] maxAmountsIn;
+        bytes userData;
+        bool fromInternalBalance;
+    }
+
+    enum JoinKind { 
+    INIT, 
+    EXACT_TOKENS_IN_FOR_BPT_OUT, 
+    TOKEN_IN_FOR_EXACT_BPT_OUT
+ }
+
+    
+ function getPoolId() external view returns (bytes32);
+
+ 
+
+
+}
+
+interface IAsset {
+   
+}
+
+
+
